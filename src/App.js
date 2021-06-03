77698d60366a96119f711b4d847778dd0510722a
Hosam Elshazly <hosamelshazly92@gmail.com>
2021-06-03 13:57:45 +0200

    define search component

--- src/App.js
+++ src/App.js
@@ -7,30 +7,43 @@ const App = () => {
   const appKey = "6250b2ebd7cfe37e116e92645f480783";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
 
   useEffect(() => {
     const getRecipes = async () => {
-      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`);
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`);
       const data = await response.json();
       setRecipes(data.hits);
       // console.log(data.hits);
     }
 
     getRecipes();
-  }, []);
+  }, [query]);
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  }
+
+  const handleSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
 
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text" />
+      <form className="search-form" onSubmit={ handleSearch }>
+        <input className="search-bar" type="text" value={search} onChange={ updateSearch } />
         <button className="search-button" type="submit">Search</button>
       </form>
       { recipes.map((itm, idx) => (
-        <Recipe 
+        <Recipe
+          key={ idx }
           title={ itm.recipe.label }
           calories={ itm.recipe.calories }
           img={ itm.recipe.image }
-          key={ idx }
+          ingredients={ itm.recipe.ingredients }
         />
       )) }
     </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Recipe = ({ title, calories, img }) => {
+const Recipe = ({ title, calories, img, ingredients }) => {
     return (
         <div>
             <h2>{ title }</h2>
+            <ul>
+                { ingredients.map((itm, idx) => <li key={ idx }>{ itm.text }</li>) }
+            </ul>
             <p>{ calories }</p>
             <img src={ img } alt="recipe food" />
         </div>
